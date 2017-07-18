7a1dfa571744e153d46d34a949033fafc1a2aed0
Vlad <tvl95@bk.ru>
2017-07-18 15:55:23 +0300

    jqmobile

--- lesson1/js/main.js
+++ lesson1/js/main.js
@@ -45,10 +45,56 @@ function startTime() {
 function change(){
 	$('#two').css({color:'green','font-size': '10px'});
 }
+function draw(){
+	var mycanvas = document.getElementById("draw");
+	var tmp=mycanvas.getContext('2d');
+	tmp.font='30px Verdana';
+	tmp.fillStyle='#000000';
+	tmp.fillText("Tizen", 10, 30); 
+	tmp.font='30px Verdana';
+	tmp.fillStyle='#60016d';
+	tmp.fillText("Javascript", 10, 60);
+	tmp.font='30px Verdana';
+	tmp.fillStyle='#60016d';
+	tmp.fillText("canvas", 39, 117);
+	tmp.font='30px Verdana';
+	tmp.fillStyle='#ff0100';
+	tmp.fillText("HTML5", 10, 110);
+}
+function drawChess(){
+	var mycanvas = document.getElementById("draw2");
+	var tmp=mycanvas.getContext('2d');
+	tmp.fillRect(10,10,160,160);
+	tmp.fillStyle='#ffffff';
+	for(i=0;i<8;i++)
+		for(j=0;j<8;j++){
+			if((i+j) % 2 == 0 )
+			tmp.fillRect(10+i*20,10+j*20,20,20);
+		}
+}
+function loadCanvas3(){
+	var mycanvas = document.getElementById("draw3");
+	var tmp=mycanvas.getContext('2d');
+	tmp.fillStyle='#ffffff';
+	tmp.fillRect(0,0,300,200);
+}
 function checkTime(i) {
 	if (i < 10) {
 		i="0" + i;
 	}
 	return i;
+}
+function animat(){
+	var start = new Date();
+	var mycanvas = document.getElementById("draw3");
+	var tmp=mycanvas.getContext('2d');
+	var speed=0.008
+	for (var i = 0; i<2000; i++)
+	{
+		tmp.fillStyle='#ffffff';
+		tmp.fillRect(0,0,300,200);
+		tmp.fillStyle='#ff0000';
+		tmp.fillRect(i*speed+20,40,20,20);
+	}
 }
 
